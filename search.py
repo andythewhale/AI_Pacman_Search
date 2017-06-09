c5247f2b9a0322316fcb46722bf0da3985a76d03
Andy Miller <andymiller1313@gmail.com>
2017-06-08 20:47:57 -0500

    Updated and fixed some words to have everything make more sense.

--- search.py
+++ search.py
@@ -105,25 +105,28 @@ def depthFirstSearch(problem):
     frontier = util.Stack()
 
     # Set just eliminates duplicate values.
-    expanded = set()
+    # This is where our visited stuff goes.
+    visited = set()
 
     # this pushes the start state onto our frontier to start us.
     # it assigns an empty list with the score zero as well.
+
     frontier.push((problem.getStartState(), [], 0))
 
     # This is the magic part.
     # I got this part right minus the above within the loop.
+    # while we do not have an frontier ...
     while not frontier.isEmpty():
         stata, successors, costa = frontier.pop()
 
         # NOTE: this is for when stuff is added to the recursive function.
         # Initially this will not be implemented.
         # NOTE: that continue reissues the while loop beginning. I learned this (:
-        if (stata in expanded):
+        if (stata in visited):
             continue;
 
-        # so when we don't continue, we add the state to the expanded set.
-        expanded.add(stata)
+        # so when we don't continue, we add the state to the visited set.
+        visited.add(stata)
 
         # So if we somehow get the goal... (The dots are the goal :))
         if problem.isGoalState(stata):
@@ -165,7 +168,63 @@ def breadthFirstSearch(problem):
   [2nd Edition: p 73, 3rd Edition: p 82]
   """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+
+    # So the difference in BFS and DFS is just how we prioritize our frontier.
+    # Luckyily, we are given a FIFO function. Queue. The rest has already been defined in DFS.
+
+    frontier = util.Queue()
+
+    # Set just eliminates duplicate values.
+    visited = set()
+
+    # this pushes the start state onto our frontier to start us.
+    # it assigns an empty list with the score zero as well.
+    frontier.push((problem.getStartState(), [], 0))
+
+    # This is the magic part.
+    # I got this part right minus the above within the loop.
+    while not frontier.isEmpty():
+        stata, successors, costa = frontier.pop()
+
+        # NOTE: this is for when stuff is added to the recursive function.
+        # Initially this will not be implemented.
+        # NOTE: that continue reissues the while loop beginning. I learned this (:
+        if (stata in visited):
+            continue;
+
+        # so when we don't continue, we add the state to the visited set.
+        visited.add(stata)
+
+        # So if we somehow get the goal... (The dots are the goal :))
+        if problem.isGoalState(stata):
+            return successors
+
+        # This part was extremely helpful and I took it from the stack article.
+        for state, direction, cost in problem.getSuccessors(stata):
+            frontier.push((state, successors + [direction], costa))
+
+    return [];
+
+
+"""
+Breadth-First-Search(Graph, root):
+    
+    create empty set S
+    create empty queue Q      
+
+    add root to S
+    Q.enqueue(root)                      
+
+    while Q is not empty:
+        current = Q.dequeue()
+        if current is the goal:
+            return current
+        for each node n that is adjacent to current:
+            if n is not in S:
+                add n to S
+                n.parent = current
+                Q.enqueue(n)
+"""
 
 
 def uniformCostSearch(problem):
